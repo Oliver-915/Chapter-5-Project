b27b23e1b68e6c69a7e1f4d840dbcefc739c1a77
Oliver-915 <oliver.albert915@gmail.com>
2025-10-22 12:49:18 -0500

    Update Number guessing game.py

--- Number guessing game.py
+++ Number guessing game.py
@@ -19,19 +19,19 @@ def newgame():
     name = input("Please enter your name")
     print("Would you like to change the range? The default is 1-100")
     range_change = input("Yes or No?: ")
-    if range_change = Yes or yes:
+    if range_change == Yes or yes:
         range()
     else: print("The range will be 1-1000")
     gamestart()
 
 def gamestart():
-    #gamestart here
+    pass
 
 def range():
-    numrange_min = 1
-    numrange_max = 100
-    numrange_min = int(input("Please enter the min number range, default is 1"))
-    numrange_max = int(input("Please enter the max number range, default is 1000"))
+    numrange_min == 1
+    numrange_max == 100
+    numrange_min == int(input("Please enter the min number range, default is 1"))
+    numrange_max == int(input("Please enter the max number range, default is 1000"))
 
 def exit():
     print("You are now exiting The Number Guessing Game:")
