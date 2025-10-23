8b2b54827af6bed752d3d370395c5dd862f6a9e0
Bricen-W10 <bgw1986@icloud.com>
2025-10-23 13:05:14 -0500

    Update Number guessing game.py

--- Number guessing game.py
+++ Number guessing game.py
@@ -1,6 +1,7 @@
 #Number guessing game
 #Oliver and Brycen
-
+import math
+import random
 def main():
     menu()
         
@@ -9,50 +10,57 @@ def menu():
     print("Main Menu")
     print("Please choose one of the following")
     print("1 | Start New Game")
+    print('2 | Choose Range')
     print('2 | Exit')
-    option = int(input("Chose a number 1 or 2: "))
+    option = int(input("Chose a number 1, 2 or 3: "))
     if option == 1:
-        newgame()
+        newgame(p1, p2, mini, maxi)
     elif option == 2:
+        get_range_and_random()
+    elif option == 3:
         exit()
     else:
-        print("Choose a number 1 or 2.")
+        print("Choose a number 1, 2 or 3.")
         
-def newgame():
-    name = input("Please enter your name")
-    print("Would you like to change the range? The default is 1-100")
-    range_change = input("Yes or No?: ")
-    if range_change == Yes or yes:
-        range()
-    else: print("The range will be 1-1000")
-    gamestart()
 
-<<<<<<< Updated upstream
-def gamestart():
-<<<<<<< HEAD
-    #gamestart here
-    pass
-def get_random():
+def newgame(p1, p2, mini, maxi):
+    p1 = ''
+    p2 = ''
     mini = 1
     maxi = 1000
-=======
-def get_random():
-    numrange_min = 1
-    numrange_max = 1000
->>>>>>> Stashed changes
-    mini = int(input('Enter the minimum range: '))
-    maxi = int(input('Enter the maximum range: '))
+    get_names()
+    get_range_and_random()
+    p1guess = int(input(f'{player1} guess your number inbetween {mini} and {maxi}: '))
     
-    random_int = random.randint(mini, maxi)
-    
-    return random_int
-    
-    
->>>>>>> Stashed changes
-=======
-    pass
 
->>>>>>> 71b3bbccbc31b91c07b2a99681c364e712a3470f
+
+
+
+def get_names():
+    p1 = input('Enter player 1s name: ')
+    p2 = input('Enter player 2s name: ')
+    return p1
+    return p2
+
+    
+    
+    
+
+def get_range_and_random():
+    mini = float(input('Enter the minimum range integer: '))
+    maxi = float(input('Enter the maximum range integer: '))
+    return mini
+    return maxi
+    rand = 1
+    rand = random.randint(mini, maxi)
+    return rand
+
+
+    
 
 def exit():
     print("You are now exiting The Number Guessing Game:")
+
+
+
+main()
